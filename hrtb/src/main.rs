9795d1b4d2d16bcbf50a14d823073e74403f122c
sergey-shandar <sergey-shandar@users.noreply.github.com>
2023-12-31 21:37:31 -0800

    MyFn

---
+++ hrtb/src/main.rs
@@ -0,0 +1,60 @@
+struct Closure<F> {
+    data: (u8, u16),
+    func: F,
+}
+
+impl<F> Closure<F>
+    where F: Fn(&(u8, u16)) -> &u8,
+{
+    fn call_0(&self) -> &u8 {
+        (self.func)(&self.data)
+    }
+}
+
+// HRTB with explicit lifetime
+
+impl<F> Closure<F>
+    where for<'a> F: Fn(&'a (u8, u16)) -> &'a u8,
+{
+    fn call_1(&self) -> &u8 {
+        (self.func)(&self.data)
+    }
+}
+
+// HRTB for my own trait
+
+trait MyFn<Args> {
+    type Output;
+    fn call(&self, args: Args) -> Self::Output;
+}
+
+impl<F> Closure<F>
+    where for<'a> F: MyFn<&'a (u8, u16), Output = &'a u8>
+{
+    fn call_2(&self) -> &u8 {
+        self.func.call(&self.data)
+    }
+}
+
+fn do_it(data: &(u8, u16)) -> &u8 { &data.0 }
+
+struct DoIt();
+
+impl<'a> MyFn<&'a (u8, u16)> for DoIt {
+    type Output = &'a u8;
+    fn call(&self, args: &'a (u8, u16)) -> Self::Output {
+        &args.0
+    }
+}
+
+fn main() {
+    {
+        let clo = Closure { data: (0, 1), func: do_it };
+        println!("{}", clo.call_0());
+        println!("{}", clo.call_1());
+    }
+    {
+        let clo = Closure { data: (0, 1), func: DoIt() };
+        println!("{}", clo.call_2());
+    }
+}
