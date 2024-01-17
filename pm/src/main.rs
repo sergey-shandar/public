9f1e2b49a15e8f0002eb3a7292ccd1ef79381245
sergey-shandar <sergey-shandar@users.noreply.github.com>
2024-01-17 05:21:19 -0800

    fixed

--- pm/src/main.rs
+++ pm/src/main.rs
@@ -2,13 +2,14 @@ const ANSWER_TO_QUESTION: u32 = 42;
 
 fn main() {
     match 5 {
-        ANSWER_TO_QESTION => println!("5 is not the answer!"),
+        ANSWER_TO_QUESTION => println!("5 is not the answer!"),
         _ => println!("Ok"),
     }
 }
 
 #[cfg(test)]
 mod test {
+    use super::*;
     #[test]
     fn test() {
         match 5 {
