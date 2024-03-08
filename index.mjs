504c64495a3a598403af2a09d42addad47e76581
sergey-shandar <sergey-shandar@users.noreply.github.com>
2024-03-08 11:07:38 -0800

    compilation error

--- index.mjs
+++ index.mjs
@@ -1,3 +1,5 @@
+// Mutable:
+
 /**
  * @typedef {{
  *  a: string
@@ -15,11 +17,31 @@ const f = a => {
  * }} AA
  */
 
-/** @type {A} */
+/** @type {AA} */
 const aa = { a: 'a' }
 
+// `aa.a = 'b'` after this call but we have no compile errors
 f(aa)
 
-if (aa.a !== 'a') {
-    throw new Error('Invalid AA object.')
-}
+// Immutable:
+
+/**
+ * @typedef {{
+ *  readonly a: string
+ * }} RA
+ */
+
+/** @type {(a: RA) => RA} */
+const rf = a => ({ ...a, a: 'b' })
+
+/**
+ * @typedef {{
+ *  readonly a: "a"
+ * }} RAA
+ */
+
+/** @type {RAA} */
+let raa = { a: 'a' }
+
+// // a compile error: Type 'RAA' is not assignable to type 'RA'
+// raa = rf(raa)
